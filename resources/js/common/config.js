03e0984f24c0879bba51ed30d4d9671f5f924350
PlodHL <dxhl1@163.com>
2016-04-26 18:16:45 +0800

    全局变量服务器访问地址

--- resources/js/common/config.js
+++ resources/js/common/config.js
@@ -1,7 +1,7 @@
 define(function (require, exports, module) {
     var UTIL = require("common/util.js");
 
-    exports.serverRoot = 'http://192.168.18.166';
+    exports.serverRoot = 'http://imsoperate.cleartv.cn';
     exports.Resource_UploadURL = "http://imsresource.cleartv.cn/upload";
     exports.projectName = UTIL.getCookie('project_name');
     exports.termListLoadInterval = 60 * 1000;
