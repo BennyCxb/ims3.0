7858b49a74d84895d1be73803a9d419882c2bdec
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-04-29 15:40:22 +0800

    2016-04-29 陈晓斌

--- resources/js/pages/channel/mtrCtrl.js
+++ resources/js/pages/channel/mtrCtrl.js
@@ -503,15 +503,15 @@ define(function (require, exports, module) {
         })
         //播放顺序
         $("#mtrCtrl_playType").change(function () {
-            playTypeSave(this);
+            playTypeSave();
         })
     }
 
     //播放顺序保存
-    function playTypeSave (self) {
+    function playTypeSave () {
         if (!inputCheck()) return;
         var overall_schedule_params = {
-            Type: $(self).val()
+            Type: $("#mtrCtrl_playType").val()
         };
         DB.collection("widget").update({overall_schedule_params: JSON.stringify(overall_schedule_params)}, {id: Number($("#mtrCtrl_Title").attr("widget_id"))});
     }
