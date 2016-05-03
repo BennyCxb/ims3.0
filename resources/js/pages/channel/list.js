e5532b242a67986d2924bd4b4213a8f6db1a33d2
kunmingren <834719922@qq.com>
2016-05-03 10:15:06 +0800

    频道审核优化

--- resources/js/pages/channel/list.js
+++ resources/js/pages/channel/list.js
@@ -73,8 +73,7 @@ define(function(require, exports, module) {
 					//审核通过
 					$('#chn_pass').click(function(){
 						
-						if(!$('#chn_pass').attr('disabled')){
-							
+						if(!$('#chn_pass').attr('disabled')){	
 							var data = {
 							  "project_name": config.projectName,
 							  "action": "checkPass",
@@ -86,13 +85,14 @@ define(function(require, exports, module) {
 								JSON.stringify(data),
 								function(data){
 									if(data.rescode === '200'){
-										alert('审核');
+										alert('审核通过');
 										loadPage(1);
 									}else{
 										alert('审核失败');
 									}
-								}
+								}		
 							)
+							alert('处理中...');
 						}
 					})
 		
