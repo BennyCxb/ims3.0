4a5f6d8b6f7c66dd21be0c2f0793889ec5d638ce
PlodHL <dxhl1@163.com>
2016-05-10 09:25:44 +0800

    源码移植到18.202

--- resources/js/common/config.js
+++ resources/js/common/config.js
@@ -2,8 +2,10 @@ define(function (require, exports, module) {
     var UTIL = require("common/util.js");
 
     exports.userName = $('#USER-NAME').html();
-    exports.serverRoot = 'http://imsoperate.cleartv.cn';
-    exports.Resource_UploadURL = "http://imsresource.cleartv.cn/upload";
+    exports.serverRoot = 'http://192.168.18.202';
+    exports.Resource_UploadURL = "http://192.168.18.202/upload";
+    //exports.serverRoot = 'http://imsoperate.cleartv.cn';
+    //exports.Resource_UploadURL = "http://imsresource.cleartv.cn/upload";
     exports.projectName = UTIL.getCookie('project_name');
     // exports.termListLoadInterval = 60 * 1000;
     exports.termSnapInterval = 1 * 1000;
