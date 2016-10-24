413ef8eb237b72bb8331ef734b61e82be8ed3801
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-10-24 10:39:21 +0800

    2016-10-24 cxb 多语言

--- resources/js/config.js
+++ resources/js/config.js
@@ -2,5 +2,5 @@ var CONFIG = {
 	"requestURL" : "http://172.17.173.103",
 	"uploadURL" : "http://172.17.173.103/upload",
 	"version" : "3.0.1",						//版本
-	"default_language" : "zh-CN"						//默认语言
+	"default_language" : "zh-CN"				//默认语言
 }
