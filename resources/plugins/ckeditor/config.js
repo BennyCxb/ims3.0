875b4e390d48596e53841831db6d8559aae48494
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-12-29 14:11:56 +0800

    修改字体大小选择配置

--- resources/plugins/ckeditor/config.js
+++ resources/plugins/ckeditor/config.js
@@ -1,22 +1,27 @@
 ﻿/*
-Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+ Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
 
-CKEDITOR.editorConfig = function( config ) {
-config.font_names='宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;'+ config.font_names;
-config.toolbar =
-[
-    ['Source'],
-    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-    ['Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
-    '/',
-    ['Styles','Format','Font','FontSize'],
-    ['TextColor','BGColor'],
-    ['Maximize', 'ShowBlocks','-','Undo','Redo']
+CKEDITOR.editorConfig = function (config) {
+    config.font_names = '宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;' + config.font_names;
+    config.toolbar =
+        [
+            ['Source'],
+            ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'],
+            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak'],
+            '/',
+            ['Styles', 'Format', 'Font', 'FontSize'],
+            ['TextColor', 'BGColor'],
+            ['Maximize', 'ShowBlocks', '-', 'Undo', 'Redo']
 
-];
+        ];
+//字体编辑时可选的字体大小 plugins/font/plugin.js
+    config.fontSize_sizes = '8/8px;9/9px;10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;' +
+        '28/28px;30/30px;32/32px;34/34px;36/36px;38/38px;40/40px;42/42px;44/44px;46/46px;48/48px;50/50px;52/52px;54/54px;' +
+        '56/56px;58/58px;60/60px;62px;64/64px;66/66px;68/68px;70/70px;72/72px;74/74px;76/76px;78/78px;80/80px;82/82px;' +
+        '84/84px;86/86px;88/88px;90/90px;92/92px;94/94px;'
 };
